0985c2df46d137a77aa0391deb32348afcb8eceb
matepal00 <mateusz.paluch2311@gmail.com>
2025-03-14 18:28:54 +0100

    adjust wallets

--- src/utils/web3/wallet.ts
+++ src/utils/web3/wallet.ts
@@ -1,6 +1,5 @@
 import { WalletAdapter } from './adapters/types'
 import { NightlyWalletAdapter } from './adapters/nightly'
-import { SalmonWalletAdapter } from './adapters/salmon'
 import { BackpackWalletAdapter } from './adapters/backpack'
 import { WalletType } from '@store/consts/types'
 import { sleep } from '@invariant-labs/sdk-sonic'
@@ -21,9 +20,6 @@ const connectStaticWallet = async (wallet: WalletType) => {
   switch (wallet) {
     case WalletType.BACKPACK:
       _wallet = new BackpackWalletAdapter()
-      break
-    case WalletType.SALMON:
-      _wallet = new SalmonWalletAdapter()
       break
     case WalletType.NIGHTLY_WALLET:
       _wallet = new NightlyAdapter()
